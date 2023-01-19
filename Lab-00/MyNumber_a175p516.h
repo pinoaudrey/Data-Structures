b11307af648d229622a473b66270e2a72accb90f
pinoaudrey <91105614+pinoaudrey@users.noreply.github.com>
2023-01-19 13:15:18 -0600

    Add files via upload

---
+++ Lab-00/MyNumber_a175p516.h
@@ -0,0 +1,77 @@
+#ifndef __MYNUMBER_H__
+#define __MYNUMBER_H__
+
+#include <algorithm>
+#include <iostream>
+using namespace std;
+
+template <typename DataType>
+class MyNumber
+{
+  public:
+    explicit MyNumber(DataType rhs = 0) // default constructor
+    {
+        // code begins
+        num = new DataType{rhs};
+        // code ends
+    }
+    
+    MyNumber(const MyNumber<DataType> & rhs)	// copy constructor with a MyNumber instance
+    {
+        // code begins
+        num = new DataType{*rhs.num};
+        // code ends
+    }
+    
+    MyNumber(MyNumber<DataType> && rhs)	// move constructor with a MyNumber instance
+    {
+        // code begins
+        rhs.num = nullptr;
+        // code ends
+    }
+    
+    MyNumber & operator= (const MyNumber<DataType> &rhs)	// copy assignment with a MyNumber instance
+    {
+        // code begins
+        MyNumber copy = rhs;
+        swap(*this,copy);
+        return *this;
+        // code ends
+    }
+
+    MyNumber & operator= (MyNumber<DataType> && rhs)	// move assignment with a MyNumber instance 
+    {
+        // code begins
+        swap(num, *rhs.num);
+        return *this;
+        // code ends
+    }
+
+    ~MyNumber(void) // destructor
+    {
+        // code begins
+        delete num;
+        // code ends
+    }
+    
+    DataType read(void) const
+    {   
+        // code begins
+        return *num;
+        // code ends
+    }
+
+    void write(DataType rhs)
+    {
+        // code begins
+        *num = rhs;
+        // code ends
+    }
+
+  private:
+    /* data */
+    DataType *num;
+};
+
+
+#endif // __MYVECTOR_H__
